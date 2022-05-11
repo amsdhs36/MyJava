f9407a7a8aa0d1f8caaf0c084c4617425aaa418c
Amruta Singh <amrutasinghpatil@gmail.com>
2022-05-11 17:22:41 +0530

    Changes have been committed

---
+++ CamelCase.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class CamelCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       //implicit conversion
+		System.out.println("Implicit Type casting");
+		char a='A';
+		System.out.println("Value of a:"+a);
+		int b=a;
+		System.out.println("Value of b:"+b);
+		float c=a;
+		System.out.println("Value of c:"+c);
+		long d=a;
+		System.out.println("Value of d:"+d);
+		double e=a;
+		System.out.println("Value of e:"+e);
+		System.out.println("\n");
+		System.out.println("Explicit type casting");
+	  //Explicit Conversion
+		double x=45.5;
+		int y=(int)x;
+		System.out.println("Value of x:"+x);
+		System.out.println("Value of y:"+y);
+	}
+
+}
